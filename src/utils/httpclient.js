b4fb2549e3289c8fdb1f17a55b4c55dff3e285bd
luistorresse <luistorresse97@gmail.com>
2022-06-28 18:00:49 -0500

    master

--- src/utils/httpclient.js
+++ src/utils/httpclient.js
@@ -3,7 +3,7 @@ const API_TOKEN = process.env.REACT_APP_API_TOKEN
 export function HttpClient(path){
     return fetch(API + path,{
         headers: {
-            authorization: "Bearer " + API_TOKEN ,
+            authorization: "Bearer" + API_TOKEN ,
             "Content-type": "application/json;charset=utf-8"}
     }).then(res => res.json())
 
